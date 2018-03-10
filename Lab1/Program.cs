64c97107f31e9c4ca40682dd8d587b25720d9459
Hong-Jie Dai <hjdai@nttu.edu.tw>
2018-03-10 16:54:50 +0800

    加入範例程式與資料

---
+++ Lab1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader(@"..\..\..\Dataset\0005_gs.xml"))
+            {
+                while (sr.Peek() != -1)
+                {
+                    string line = sr.ReadLine();
+                    foreach (var ch in line)
+                    {
+                        if (!Char.IsWhiteSpace(ch))
+                        {
+                            Console.Write(ch);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
